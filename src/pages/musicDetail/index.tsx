73b49caa79975416fe4022c01a16229cb2595537
SHERlocked93 <443033099@qq.com>
2018-12-15 15:53:32 +0800

    test: test commit

--- src/pages/musicDetail/index.tsx
+++ src/pages/musicDetail/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const MusicDetail = (props = {}) => {
-    return <h1>this is MusicDetail page</h1>;
+    return <h1>this is MusicDetail page.</h1>;
 };
 
 export default MusicDetail;
