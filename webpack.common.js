b8f9163627471e46063c55c8047a0a0899bf64b1
SHERlocked93 <443033099@qq.com>
2018-12-15 19:11:09 +0800

    feat: 1. 增加favicon 2. 增加webpack-dev-server，但是热更新不太行，现在是直接刷新页面，求大佬帮！

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,15 @@ const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
-const host = require('ip').address()
 
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
 
 module.exports = {
-    entry: {
-        main: [resolve('src/App.tsx')]
-    },
+    entry: [
+        resolve('src/App.tsx')
+    ],
     output: {
         path: resolve('dist'),
         filename: '[name].js'
@@ -25,7 +24,6 @@ module.exports = {
     },
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             {
                 test: /\.tsx?$/,
                 loader: "awesome-typescript-loader"
@@ -41,7 +39,7 @@ module.exports = {
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
                 })
-            }, , {
+            }, {
                 test: /\.(png|jpe?g|gif)$/,
                 use: [{
                     loader: 'url-loader', options: { limit: 8192, name: 'res/[name].[ext]' }
@@ -67,7 +65,8 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "音乐播放器",
-            template: "index.html"
+            favicon: './favicon.ico',
+            template: "./index.html"
         }),
         new ExtractTextPlugin('index.css')
     ]
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,30 +1,55 @@
-const merge = require('webpack-merge'),
-    common = require('./webpack.common.js'),
-    path = require('path'),
-    webpack = require('webpack'),
-    getAbsolutePath = (relativePath) => {
-        return path.resolve(__dirname, relativePath)
-    }
-// webpackHotMiddleware = 'webpack-hot-middleware/client?path=/__webpack_hmr&reload=true';
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const path = require('path')
+const webpack = require('webpack')
+const packageConfig = require('./package.json')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
+// 配置
+const host = require('ip').address()
+const port = 9903
 
 const devConfig = merge(common, {
-    entry: ['webpack-hot-middleware/client?noInfo=true&reload=true', getAbsolutePath('src/App.tsx')],
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
+        host, port,
+        clientLogLevel: 'warning',
+        // hot: true,           // 大佬帮弄一下
+        quiet: true,
         contentBase: "./dist",
         compress: true,
-        port: 9903,
         publicPath: '/'
     },
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
+        new FriendlyErrorsPlugin({
+            compilationSuccessInfo: {
+                messages: [`Your application is running here: http://${host}:${port}`]
+            },
+            onErrors: createNotifierCallback()
+        })
     ]
-});
+})
 
-// Object.keys(devConfig.entry).forEach((key, i) => {
-//     devConfig.entry[key].push(webpackHotMiddleware);
-// })
-module.exports = devConfig;
+function createNotifierCallback() {
+    const notifier = require('node-notifier')
+
+    return (severity, errors) => {
+        if (severity !== 'error') return
+
+        const error = errors[0]
+        const filename = error.file && error.file.split('!').pop()
+
+        notifier.notify({
+            title: packageConfig.name,
+            message: severity + ': ' + error.name,
+            subtitle: filename || '',
+            icon: path.join(__dirname, 'logo.png')
+        })
+    }
+}
+
+module.exports = devConfig
